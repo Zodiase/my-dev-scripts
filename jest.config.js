07277f9ddbe6178de9f90850ceee98a1b3488de3
Xingchen Hong <hello@xc-h.com>
2020-02-17 23:49:22 -0800

    Enable root alias.
    - `tsconfig.compilerOptions.paths` is used to config typescript for the alias.
    - `jest.config.moduleNameMapper` is used to config jest for the alias.
    - `*.spec.ts` must not be excluded so root alias works in test files.
    - `ts-node`, `nodemon` and "start" script are removed since they don't serve a great purpose.

--- jest.config.js
+++ jest.config.js
@@ -72,7 +72,9 @@ module.exports = {
     moduleFileExtensions: ['js', 'ts'],
 
     // A map from regular expressions to module names that allow to stub out resources with a single module
-    // moduleNameMapper: {},
+    moduleNameMapper: {
+        '~/(.*)': '<rootDir>/$1',
+    },
 
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
     // modulePathIgnorePatterns: [],
